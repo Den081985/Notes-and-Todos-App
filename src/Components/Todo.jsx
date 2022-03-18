4420a614542fcd3c7599c5c52c2816bda9723e0e
Den081985 <pishnenkod@gmail.com>
2022-03-18 18:17:24 +0300

    добавлены editTemplate,submitHandler

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,29 +1,114 @@
-import React from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 
-export const Todo = ({ tasks }) => {
-  return (
-    <ul className="list-group">
-      {tasks.map((task) => (
-        <li className="list-group-item todo" key={task.id}>
-          <span className="todo-span">
-            <input
-              className="form-check-input me-1"
-              type="checkbox"
-              value=""
-              aria-label="..."
-            />
-            {task.name}
-          </span>
-          <div className="todo-btns">
-            <button type="button" className="btn btn-dark todo-edit">
-              Edit
-            </button>
-            <button type="button" className="btn btn-dark todo-remove">
-              &times;
-            </button>
+//кастомный хук для получения предыдущего состояния
+export const usePrevious = (value) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+export const Todo = (props) => {
+  const [isEditing, setEditing] = useState(false);
+
+  const [newName, setNewName] = useState("");
+
+  const buttonRef = useRef(null);
+  const inputRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    props.editTodo(props.id, newName);
+    setNewName("");
+    setEditing(false);
+  };
+  const viewTemplate = (
+    <Fragment>
+      <span className="todo-span">
+        <input
+          id={props.id}
+          className="form-check-input me-1"
+          type="checkbox"
+          value=""
+          aria-label="..."
+          defaultChecked={props.completed}
+          onChange={() => props.toogleCompleted(props.id)}
+        />
+        {props.name}
+      </span>
+      <div className="todo-btns">
+        <button
+          type="button"
+          className="btn btn-dark todo-edit"
+          onClick={() => setEditing(true)}
+          ref={buttonRef}
+        >
+          Edit
+        </button>
+        <button
+          type="button"
+          className="btn btn-dark todo-remove"
+          onClick={() => props.remove(props.id)}
+        >
+          &times;
+        </button>
+      </div>
+    </Fragment>
+  );
+
+  const editTamplate = (
+    <form onSubmit={submitHandler}>
+      <div className="todo-form">
+        <div className="mb-3">
+          <label htmlFor={props.id} className="form-label">
+            New name for {props.name}
+          </label>
+          <input
+            id={props.id}
+            type="text"
+            className="form-control"
+            aria-describedby="emailHelp"
+            value={newName}
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <div id="emailHelp" className="form-text">
+            Input new name for a todo item
           </div>
-        </li>
-      ))}
-    </ul>
+        </div>
+        <div className="edit-btns">
+          <button type="submit" className="btn btn-dark save-btn">
+            Save
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark cancel-btn"
+            onClick={() => setEditing(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      inputRef.current.focus();
+    } else if (wasEditing && !isEditing) {
+      buttonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
+
+  return (
+    <li className="list-group-item todo" key={props.id}>
+      {isEditing ? editTamplate : viewTemplate}
+    </li>
   );
 };
