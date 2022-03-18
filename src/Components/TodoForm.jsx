d05d6b688728c71f423f4e9787ed2867f49fe6e9
Den081985 <pishnenkod@gmail.com>
2022-03-18 18:18:19 +0300

    добавлен submitHandler

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TodoForm = () => {
-  return <div></div>;
+export const TodoForm = ({ addTodo }) => {
+  const [name, setName] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    addTodo(name);
+    setName("");
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-floating mb-4">
+        <input
+          type="text"
+          className="form-control"
+          id="floatingPassword"
+          placeholder="Input your todo"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="floatingPassword">Todo</label>
+      </div>
+    </form>
+  );
 };
