12f003c0a4a83ac834f31e13c65afcc1b572b435
Den081985 <pishnenkod@gmail.com>
2022-03-19 17:38:17 +0300

    добавлен CSSTransition

--- src/Components/Alert.jsx
+++ src/Components/Alert.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { useContext } from "react";
+import { CSSTransition } from "react-transition-group";
 import { AlertContext } from "../Context/Alert/alertContext";
 
 export const Alert = () => {
   const { alert, hide } = useContext(AlertContext);
   if (!alert.visible) {
     return null;
-  }
+  } //in-принимает условие отображения компонента и запускает состояния входа или выхода
+
   return (
-    <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
-      <strong>Holy guacamole!</strong>
-      &nbsp;{alert.text}
-      <button
-        type="button"
-        className="btn-close"
-        data-bs-dismiss="alert"
-        aria-label="Close"
-        onClick={hide}
-      ></button>
-    </div>
+    <CSSTransition
+      in={alert.visible}
+      classNames={"alert"}
+      timeout={{ enter: 500, exit: 350 }}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div
+        className={`alert alert-${alert.type || "warning"} alert-dismissible`}
+      >
+        <strong>Holy guacamole!</strong>
+        &nbsp;{alert.text}
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+          onClick={hide}
+        ></button>
+      </div>
+    </CSSTransition>
   );
 };
