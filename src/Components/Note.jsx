3880e5d4a92a624e4432b9d5e04808de048a6dc4
Den081985 <pishnenkod@gmail.com>
2022-03-19 17:40:14 +0300

    добавлен CSSTransition,TransitionGroup

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export const Note = ({ notes, removeNote }) => {
   return (
-    <ul className="list-group">
+    <TransitionGroup component={"ul"} className="list-group">
       {notes.map((note) => (
-        <li className="list-group-item note" key={note.id}>
-          <div>
-            <strong>{note.title}</strong>
-            <small>{new Date().toLocaleDateString()}</small>
-          </div>
-          <button
-            type="button"
-            className="btn btn-dark"
-            onClick={() => removeNote(note.id)}
-          >
-            &times;
-          </button>
-        </li>
+        <CSSTransition classNames={"note"} key={note.id} timeout={800}>
+          <li className="list-group-item note" key={note.id}>
+            <div>
+              <strong>{note.title}</strong>
+              <small>{new Date().toLocaleDateString()}</small>
+            </div>
+            <button
+              type="button"
+              className="btn btn-dark"
+              onClick={() => removeNote(note.id)}
+            >
+              &times;
+            </button>
+          </li>
+        </CSSTransition>
       ))}
-    </ul>
+    </TransitionGroup>
   );
 };
