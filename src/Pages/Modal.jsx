d1c561de792b017e676a012bad2140b626cecc9d
Den081985 <pishnenkod@gmail.com>
2022-03-21 21:49:40 +0300

    добавлены getModalText, removeText, closeModal

--- src/Pages/Modal.jsx
+++ src/Pages/Modal.jsx
@@ -1,26 +1,56 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { ModalBar } from "../Components/ModalBar";
-// import { ModalForm } from "../Components/ModalForm";
-// import { ModalContext } from "../Context/Modal/modalContext";
+import { ModalForm } from "../Components/ModalForm";
+import { ModalContext } from "../Context/Modal/modalContext";
 
 export const Modal = () => {
   const [modalActive, setModalActive] = useState(false);
+  //функци получения записи из хранилища
+  const getModalText = () => {
+    const text = JSON.parse(localStorage.getItem("modal") || "[]").join("");
+    return text;
+  };
+  let modaltext = getModalText();
+  const [modalText, setModalText] = useState(modaltext);
+  //функция удаления записей из хранилища
+  const removeText = () => {
+    localStorage.clear();
+    setModalText("");
+  };
+  //функция закрытия модального окна
+  const closeModal = () => {
+    setModalActive(false);
+  };
+
   return (
-    <div className="modal-container">
-      <ModalBar active={modalActive} setActive={setModalActive}>
-        <h2 className="modal-title">Modal Window</h2>
-        <p>Just modal window that you called when pushed the button!</p>
-      </ModalBar>
-      {/* <ModalForm /> */}
-      <button
-        type="button"
-        className="btn btn-dark"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-        onClick={() => setModalActive(true)}
-      >
-        Open modal
-      </button>
-    </div>
+    <ModalContext.Provider value={{ setModalText }}>
+      <div className="modal-container">
+        <ModalBar
+          active={modalActive}
+          setActive={setModalActive}
+          removeText={removeText}
+          closeModal={closeModal}
+        >
+          <h2 className="modal-title">Modal Window</h2>
+          <p className="modal-text">{modalText}</p>
+        </ModalBar>
+
+        <ModalForm />
+        <div className="modal-div">
+          <button
+            type="button"
+            className="btn btn-dark mt-3"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            onClick={() => setModalActive(true)}
+          >
+            Open modal
+          </button>
+          <p className="btn-text mt-5">
+            Push the button to see your modal message!
+          </p>
+        </div>
+      </div>
+    </ModalContext.Provider>
   );
 };
